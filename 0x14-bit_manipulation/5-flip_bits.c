49dc475832c338fe426ab8bb26d6aaf6a768cf88
Barachel Lutumba <barachellutumbanzengo@gmail.com>
2023-05-05 05:47:59 -0700

    flip bites

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* flip_bits - returns the number of bits to flip
+* @n: the first number
+* @m: the second number
+*
+* Return: the number of bits to flip
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor_result = n ^ m;
+unsigned int bit_count = 0;
+
+while (xor_result)
+{
+bit_count += xor_result & 1;
+xor_result >>= 1;
+}
+
+return (bit_count);
+}
